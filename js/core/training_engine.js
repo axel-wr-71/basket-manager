3eb03edb31faeddd3d8af08202bc88557dc421ff
axel-wr-71 <strubbe23@gmail.com>
2026-01-15 17:50:00 +0100

    Update training_engine.js

--- js/core/training_engine.js
+++ js/core/training_engine.js
@@ -1,11 +1,13 @@
 // js/core/training_engine.js
 
-export async function runAutomaticTraining(seasonId) {
-    // 1. Pobierz wszystkie zespoły i ich ustawione plany treningowe
-    // 2. Pobierz wszystkich zawodników
-    // 3. Dla każdego zawodnika przelicz rozwój:
-    //    Wzrost = (TypTreningu * Baza) + (Potencjał * ModyfikatorWieku) + (Lojalność)
-    
-    console.log("Symulacja treningu w toku...");
-    // Tutaj wejdzie silnik, który wymyślimy w następnym kroku
+function getSkillsByFocus(focus) {
+    const map = {
+        'SHARP_SHOOTER': ['skill_2pt', 'skill_3pt', 'skill_ft'],
+        'PAINT_PROTECTOR': ['skill_blk', 'skill_1v1d'],
+        'PERIMETER_DEFENDER': ['skill_stl', 'skill_1v1d'],
+        'PLAYMAKING_FOCUS': ['skill_pas', 'skill_dri'],
+        'BIG_MAN_INSIDE': ['skill_dnk', 'skill_reb'],
+        'ISOLATION_SCORER': ['skill_1v1o', 'skill_sta']
+    };
+    return map[focus] || [];
 }
